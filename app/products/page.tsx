3907ff8fc134fadf99b3d33070bc629bf7421c52
Steven Qi <stevenqi@StevendeMacBook-Air.local>
2025-09-20 21:21:56 +0800

    add pagination

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,17 +1,29 @@
 // import Image from "next/image";
 import sampleData from '@/db/sample-data'; 
 import ProductList from '@/components/product-list';
+import Pagination from "@/components/Pagination";
 
+const PAGE_SIZE = 5;
 
-const Products = () => {
-  return(
-  <>
-  <ProductList
-  data={sampleData.products}
-  title='Product List'
-//   limit={4}
-  />
-  </>
+const Products = ({ searchParams }: { searchParams: { page?: string } }) => {
+  const currentPage = Number(searchParams?.page) || 1;
+  const totalProducts = sampleData.products.length;
+  // const totalPages = Math.ceil(totalProducts / PAGE_SIZE);
+  
+  const startIndex = (currentPage - 1) * PAGE_SIZE;
+  const endIndex = startIndex + PAGE_SIZE;
+  const paginatedProducts = sampleData.products.slice(startIndex, endIndex);
+  
+  const hasNextPage = endIndex < totalProducts;
+
+  return (
+    <>
+      <ProductList
+        data={paginatedProducts}
+        title='Product List'
+      />
+      <Pagination page={currentPage} isNext={hasNextPage} />
+    </>
   );
 };
 
